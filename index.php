45e2f742f8e8d3f948f5799d88b804ced0893213
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2023-05-08 19:17:37 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+// 连接数据库
+$conn = mysqli_connect("localhost", "username", "password", "database_name");
+
+// 检查连接是否成功
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// 如果表格不存在，创建它
+$sql = "CREATE TABLE IF NOT EXISTS short_links (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    original_link VARCHAR(255) NOT NULL,
+    short_link VARCHAR(20) NOT NULL
+)";
+
+mysqli_query($conn, $sql);
+
+// 生成短链接函数
+function generate_short_link() {
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $code = '';
+  for ($i = 0; $i < 6; $i++) { //生成6位随机码
+    $code .= $characters[rand(0, strlen($characters) - 1)];
+  }
+  return $code;
+}
+
+// 将链接插入数据库表中
+if (isset($_POST['u'])||isset($_GET['u'])) {
+    if(isset($_POST['u']))
+        $original_link = $_POST['original_link'];
+    else
+        $original_link = $_GET['u'];
+    $short_link = generate_short_link();
+    
+    $sql = "SELECT COUNT(*) as count FROM short_links WHERE original_link = '$original_link'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    if ($row['count'] == 0) {
+        $sql = "INSERT INTO short_links (original_link, short_link) VALUES ('$original_link', '$short_link')";
+        mysqli_query($conn, $sql);
+        echo("你的短链：<a href=\"{$short_link}\" target=\"_blank\">http://s.pro-ivan.com/{$short_link}</a>");
+    } else {
+        echo "该网址已经存在于数据库中：<a href=\"{$short_link}\" target=\"_blank\">http://s.pro-ivan.com/{$short_link}</a>";
+    }
+}
+
+
+// 获取原始链接并重定向用户
+if (isset($_GET['short_link'])) {
+    $short_link = $_GET['short_link'];
+    
+    $sql = "SELECT original_link FROM short_links WHERE short_link = '$short_link'";
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $original_link = $row['original_link'];
+        header("Location: $original_link");
+        exit();
+    }
+}
+
+// 关闭数据库连接
+mysqli_close($conn);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>URL Shortener</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="shortcut icon" href="/favicon.ico">
+</head>
+<body>
+    <h1>URL Shortener</h1>
+    
+    <form method="POST" action="">
+        <label for="original_link">Original Link:</label>
+        <input type="text" name="original_link" id="original_link">
+        <br><br>
+        <input type="submit" name="u" value="Shorten Link">
+    </form>
+    
+    <br><br>
+    
+    <h3>Shortened Links List:</h3>
+    
+    <?php
+    // 连接数据库
+    $conn = mysqli_connect("localhost", "username", "password", "database_name");
+    
+    // 检查连接是否成功
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    // 显示短链接列表
+    $sql = "SELECT * FROM short_links";
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) {
+        echo "<ul>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<li><a href=\"{$row['short_link']}\" target=\"_blank\">http://s.pro-ivan.com/{$row['short_link']}</a> ({$row['original_link']})</li>";
+        }
+        echo "</ul>";
+    }
+    // 关闭数据库连接
+    mysqli_close($conn);
+    ?>
+    
+    <footer>
+        <h4><a href="//pro-ivan.com" target="_blank">A <b>FREE</b> URL Shortener Create by Pro-Ivan</a></h4>
+        <p>Type your link in the textarea then click '<b>shorten link</b>', <br>or POST/GET your link to this page with the keyname '<b>u</b>'</p>
+    </footer>
+</body>
+</html>
---
+++ redirect.php
@@ -0,0 +1,22 @@
+<?php
+// 获取短链接参数
+$short_link = $_GET['short'];
+
+// 连接到MySQL数据库
+$conn = mysqli_connect("localhost", "username", "password", "database_name");
+
+// 查询短链接对应的原始链接
+$sql = "SELECT original_link FROM short_links WHERE short_link='$short_link'";
+$result = mysqli_query($conn, $sql);
+
+// 如果找到了对应的原始链接，则进行重定向
+if (mysqli_num_rows($result) > 0) {
+  $row = mysqli_fetch_assoc($result);
+  $original_link = $row['original_link'];
+  header("Location: $original_link");
+  exit;
+} else {
+  // 如果没有找到对应的原始链接，则显示错误信息
+  echo "Error: Short link not found.";
+}
+?>
